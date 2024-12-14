3696758ac7c206f1b73275e1b2ff7be8847f6689
MahadAnsari18 <mahadansari18@gmail.com>
2024-12-14 20:33:24 +0500

    answer1

---
+++ questionNo1.dart
@@ -0,0 +1,9 @@
+// Q.1: Create a list of names and print all names using list.
+
+void main(){
+  List names = ["Wasay", "Shayan", "Mahad", "Ali"];
+  print(names);
+}
+
+// Answer is ->
+//             [Wasay, Shayan, Mahad, Ali]
