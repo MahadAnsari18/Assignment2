463c7558cfa07ec318475b0b042e7a43f7d19b27
MahadAnsari18 <mahadansari18@gmail.com>
2024-12-15 18:36:48 +0500

    Question4

---
+++ questionNo4.dart
@@ -0,0 +1,9 @@
+// Q.4: Create a list of numbers & write a program to get the smallest & greatest 
+// number from a list.
+
+void main () {
+  List numbers = [25, 75, 100, 10, 50, 15, 80,];
+  numbers.sort();
+  print("Smallest Number: ${numbers.first}");
+  print("Greatest Number: ${numbers.last}");
+}
