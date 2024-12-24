aaf7f0e067cefb20c042b6c31f93b78e4997b6c3
MahadAnsari18 <mahadansari18@gmail.com>
2024-12-24 22:44:43 +0500

    Answer13

---
+++ questionNo13.dart
@@ -0,0 +1,13 @@
+// Q.13: Implement a code that takes in a list of integers and returns a new list
+// containing only the unique elements from the original list. The order of
+// elements in the new list should be the same as in the original list.
+
+void main() {
+  List<int> digits = [5, 10, 15, 5, 18, 20, 25, 18, 10];
+  List uniqueDigits = digits.toSet().toList();
+  print(uniqueDigits);
+
+}
+
+// Answer is ->
+//            [5, 10, 15, 18, 20, 25]
